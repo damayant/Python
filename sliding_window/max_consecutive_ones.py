78ba0a2c134459069a28e5b80fdc099ab3064dd6
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-11-21 07:56:27 -0500

    correct with all corner cases covered

--- sliding_window/max_consecutive_ones.py
+++ sliding_window/max_consecutive_ones.py
@@ -3,25 +3,23 @@ from typing import List
 
 class Solution:
     def longestOnes(nums:List[int],k:int)->int:
-        start = 0
-        end = start+1
-        count = 0
-        max_count = count
-        counter = k
+        left = right = 0
+        for right in range(len(nums)):
+            print(right)
+            #if we encounter 0 then decrement k
+            if(nums[right]==0):
+                k -= 1
+            #else no impact to k
+            #if k <0 then we need to move left part of window forward to try and remove extra 0s
+            if k<0:
+                #if the left one was zero then we adjust k
+                if(nums[left]==0):
+                    k+=1
+                #regardless of whether we had a 1 or 0 we can move left side by 1
+                #if we keep seeing 1s the window still keeps moving as is
+                left += 1
+        return right - left + 1
 
-        while end<len(nums):
-            while(counter!=0):
-                if(nums[start]==1):
-                    count += 1
-                elif(nums[start]==0):
-                    count+=1
-                    counter -= 1
-                start += 1
-                end += 1
-            max_count = max(max_count,count)
-            count = 0
-            counter = k
-        print(max_count)
-        return max_count
+        
     
     longestOnes(nums=[1,1,1,0,0,0,1,1,1,1,0],k=2)
