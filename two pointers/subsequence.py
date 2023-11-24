83231522f67e604ba1c954b0ecad9617552692f2
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-11-24 11:23:36 -0500

    subsequence

---
+++ two pointers/subsequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isSubsequence(s:str,t:str)->bool:
+        left_bound , right_bound = len(s) , len(t)
+        p_left = p_right = 0
+
+        while p_left<left_bound and p_right<right_bound:
+            #move both pointers or just the right pointer
+            if(s[p_left] == t[p_right]):
+                p_left += 1
+            p_right+=1
+        
+        return p_left == left_bound
+
+
+    print(isSubsequence(s="ab",t="baab"))
+
+
+
+
+
+
+
+
+    
