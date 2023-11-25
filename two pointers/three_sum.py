d245accf2a7baeb33a5435f244baf2a8832c0fae
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-11-25 12:03:40 -0500

    added 3 sum

---
+++ two pointers/three_sum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+def threeSum(nums:List[int])->List[List[int]]:
+    res = []
+    nums.sort()
+    for i in range(len(nums)):
+        if nums[i]>0:
+            break
+        if i == 0 or nums[i-1] !=nums[i]:
+            twoSum_one(nums,i,res)
+    return res
+
+def twoSum_one(nums:List[int],i:int,res:List[List[int]]):
+    lo = i+1
+    hi = len(nums)-1
+
+    while(lo<hi):
+        sum = nums[i]+nums[lo]+nums[hi]
+        if sum < 0 :
+            lo += 1
+        elif sum >0 :
+            hi -= 1
+        else:
+            res.append([nums[i], nums[lo], nums[hi]])
+            lo += 1
+            hi -= 1
+            while lo<hi and nums[lo] == nums[lo-1]:
+                lo += 1
+
+print(threeSum(nums = [-1,0,1,2,-1,-4]))
