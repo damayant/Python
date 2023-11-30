eaa65c0d9d6c1d6236f17fb77ae93d673e7ac40b
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-11-30 08:05:00 -0500

    pandas

---
+++ pandas/combine_two_tables.py
@@ -0,0 +1,70 @@
+'''
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
+
+'''
+
+import pandas as pd
+
+def combine_two_tables(person:pd.DataFrame,address:pd.DataFrame)->pd.DataFrame:
+    # how='left' specifies that we are performing a left join, meaning all the records from the person dataframe (the left dataframe) will be retained, and the matching records from the address dataframe (the right dataframe) will be merged where the 'personId' values match. If a 'personId' from the person dataframe does not have a matching 'personId' in the address dataframe, the 'city' and 'state' columns for that record will contain NaN values (representing missing data).
+    result = pd.merge(person,address,on='personId',how='left')
+    result = result[['firstName','lastName','city','state']]
+    return result
+
