8dd59bfeda93fa5c1aae1d8b8dc7be54d0d08128
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-12-17 08:07:06 -0500

    remove nth node from end better sol added

--- linked list/remove_nth_node_from_last.py
+++ linked list/remove_nth_node_from_last.py
@@ -31,19 +31,25 @@ class Solution:
         return result.next
 
     def withOneIteration(head:Optional[ListNode],n:int)->Optional[ListNode]:
+        #initialise the dummy node 
         dummy = ListNode(0)
         dummy.next = head
 
+        #initialise the ptrs to point to dummy node
         first_ptr = dummy
         second_ptr = dummy
 
+        #shift the secnd_ptr to start n steps ahead of first ptr
         for i in range(n):
             second_ptr =  second_ptr.next
 
+        #check while the second_ptr.next is not null , shift the first and second ptr by 1 step each
         while second_ptr.next is not None:
             first_ptr =  first_ptr.next
             second_ptr = second_ptr.next
         
+        #when the second_ptr.next reaches null , the first ptr will be at a position 
+        #such that the next node to the firts ptr is the one that needs to be removed
         first_ptr.next = first_ptr.next.next
 
         return dummy.next
