3e5c091ee2ad135e4b5b03dcdfd656d87c51402a
Damayanti Ghosh <damayantighosh2@gmail.com>
2023-12-18 07:34:13 -0500

    height of bt

---
+++ binary tree/max_depth.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right =  None
+
+
+class Solution:
+    def maxDepth(root):
+        element_q = deque() 
+        element_q.append(root)
+        number_level = 0
+
+        while True :
+            node_count_at_level = len(element_q)
+            if (node_count_at_level == 0) :
+                return number_level
+            
+            while (node_count_at_level>0):
+                element = element_q.popleft()
+
+                if(element.left is not None):
+                    element_q.append(element.left)
+                if(element.right is not None):
+                    element_q.append(element.right)
+                node_count_at_level -= 1
+            
+            number_level+= 1
+
+        return number_level
+
+    
+    root = TreeNode(0)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.left.left =  None
+    root.left.right =  None
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    root.right.right.left =  TreeNode(3)
+    root.right.right.right =  TreeNode(1)
+
+    print(maxDepth(root))
+            
