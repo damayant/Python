fa78d78bdc21cedd2581c467ef2ea196f1112a52
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-01-11 16:20:57 -0500

    romn to int

---
+++ arrays/candy.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def candy(ratings: List[int]) -> int:
+        #first initialise an empty array of 1s for length of ratings arry 
+        candies = [1]*len(ratings)
+        
+        #now traverse from left to right 
+        # to make sure the candies are distributed properly comparing left neighbour
+        # leave the 0th element of array as it has no left neightbour
+        for i in range(1,len(ratings)) :
+            #if left neighbour is smaller then add 1 to the current candy-child
+            if ratings[i-1] < ratings[i] :
+                candies[i] = candies[i-1]+1 
+        
+        #now traverse right to left
+        #comparing the right neighbour
+        #leave the extreme most element ie len(ratings)-1th element as it has no right neighbour
+        for i in range(len(ratings)-2,-1,-1):
+            #check if the the right neighbour is greater
+            if ratings[i] > ratings[i+1]:
+                #only choose the heigher of the previous and increased
+                candies[i] = max(candies[i],candies[i+1]+1)
+        
+        return sum(candies)
