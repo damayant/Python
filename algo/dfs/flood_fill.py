7977d4804e4f1efbb58775bea086a05c761b40d5
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-04-24 08:25:47 -0400

    dfs

---
+++ algo/dfs/flood_fill.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def floodFill(image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        row , col = len(image), len(image[0])
+        current_color = image[sr][sc]
+
+        if current_color == color :
+            return image
+
+        
+        def dfs(r, c):
+            if image[r][c] == current_color :
+                image[r][c] = color
+
+                if r >=1:
+                    dfs(r-1,c)
+                
+                if r+1 < row :
+                    dfs(r+1,c)
+
+                if c>=1 :
+                    dfs(r,c-1)
+
+                if c+1 <col:
+                    dfs(r,c+1)
+        dfs(sr,sc)
+        print(image)
+        return image
+
+    floodFill(image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2)        
