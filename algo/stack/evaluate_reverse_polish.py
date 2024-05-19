08bb59dc32531c0cdd1fb7513b92f565c7c4f0c5
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-05-19 09:07:04 -0400

    graph

--- algo/stack/evaluate_reverse_polish.py
+++ algo/stack/evaluate_reverse_polish.py
@@ -22,5 +22,22 @@ def evalRPN(tokens:List[str])->int:
 
     return stack.pop()
 
+def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in range(len(tokens)):
+            if tokens[i] == "+":
+                stack.append(stack.pop()+stack.pop())
+            elif tokens[i] == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b-a)
+            elif tokens[i] == "*":
+                stack.append(stack.pop()*stack.pop())
+            elif tokens[i] == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+            else :
+                stack.append(int(tokens[i]))
+        return stack[0]
+
 
 print(evalRPN(tokens = ["2","1","+","3","*"]))
