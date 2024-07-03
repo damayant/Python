e92e9d65bd8cee60e2c0173cb9ed66e7e1b8dd3a
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-07-03 07:51:47 -0400

    merge account dfs

--- algo/binary serach/find_peek.py
+++ algo/binary serach/find_peek.py
@@ -2,20 +2,17 @@ from typing import List
 
 
 class Solution:
-    def findPeekElement(nums:List[int])->int:
-        if len(nums) ==  1 :
-            return 0
-    
+    def findPeakElement(self, nums: List[int]) -> int:
         left , right =  0 , len(nums)-1
 
-        while left<=right :
+        while left<right :
             mid = (left+right)//2
-            if nums[mid] > nums[mid+1]:
+            if nums[mid]>nums[mid+1]:
                 right = mid
             else:
-                left = mid + 1
+                left = mid+1
         
         return left
 
-    print(findPeekElement(nums=[3,4,3,2,1]))
+    print(findPeakElement(nums=[3,4,3,2,1]))
         
