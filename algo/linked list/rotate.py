e1a1d5a21c200ac3ed1ea8ca23f2c16e476eab7a
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-08-13 08:26:59 -0400

    dsa

--- algo/linked list/rotate.py
+++ algo/linked list/rotate.py
@@ -10,29 +10,26 @@ class Solution:
     def rotateRight(head:Optional[ListNode], k: int):
         #base cases 
         if not head :
-            return None
-        if not head.next :
             return head
+    
+        #calculate length of the linked list
+        length,tail=1,head
+        while tail.next:
+            tail=tail.next
+            length+=1
         
-        #close the linked-list into a ring
-        old_tail = head
-        n = 1
-        while old_tail.next :
-            old_tail =   old_tail.next
-            n+= 1
-        old_tail.next =  head
+        k=k%length
 
-        #find new tail : (n-k%n-1)th node
-        #add new  head : (n-k%n)th node
-        new_tail =  head
-        for i in range(n-k%n-1):
-            new_tail = new_tail.next
-        new_head = new_tail.next
+        current=head
+        for i in range(length-k-1):
+            current=current.next
+        newHead=current.next
+        current.next=None
+        tail.next=head
+        return newHead
 
-        #break the ring
-        new_tail.next = None
 
-        return new_head
+        
 
     #this solution DOES NOT pass all test cases
     def withOneIteration(head:Optional[ListNode], k: int):
