63a623ef600d061297e202b5b5f12892cef7e91c
Damayanti Ghosh <damayantighosh2@gmail.com>
2025-08-15 15:43:33 -0400

    practice

--- project/practice/authors.py
+++ project/practice/authors.py
@@ -2,20 +2,24 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 import crud,schema,database
+import logging
 
+logger=logging.getLogger(__name__)
 router=APIRouter(prefix='/authors')
 
 @router.post("/",status_code=201)
 def create_author(author: schema.AuthorCreate,db:Session=Depends(database.get_db)):
-    print(author)
+    logger.info(f"Recieved request to create author: {author.name}")
     return crud.create_author(db, author.name)
 
 @router.get("/",status_code=200)
 def get_authors(db:Session=Depends(database.get_db)):
+    logger.info(f"Generating view of authors table")
     return crud.get_all_authors(db)
 
 @router.delete("/{author_id}",status_code=204)
 def delete_author(author_id:int,db:Session=Depends(database.get_db)):
+    logger.info(f"Deleting author:{author_id}")
     crud.delete_author(db,author_id)
 
 @router.delete("/",status_code=204)
--- project/practice/crud.py
+++ project/practice/crud.py
@@ -3,25 +3,91 @@ from fastapi import HTTPException, status
 import models, schema
 import logging
 
+logger=logging.getLogger(__name__)
+
 def create_author(db: Session, name: str):
-    db_author = models.Author(name=name)
-    db.add(db_author)
-    db.commit()
-    db.refresh(db_author)
-    return db_author
+    #check if author already exist
+    existing_author=db.query(models.Author).filter(models.Author.name==name).first()
+    if existing_author:
+        logger.warning(f"Attempt to create duplicate author:{name}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Author name {name} already exists"
+        )
+    try:
+        db_author=models.Author(name=name)
+        db.add(db_author)
+        db.commit()
+        db.refresh(db_author)
+        logger.info(f"Author created: {db_author.id}-{db_author.name}")
+        return {"id":db_author.id,"name":db_author.name}
+    except Exception as e:
+        db.rollback()
+        logger.error(f"Error creating author '{name}': {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error while creating author"
+        )
+
 
 def get_all_authors(db:Session):
-    authors= db.query(models.Author).all()
-    return [{"id":author.id,"name":author.name} for author in authors]
+    try:
+        authors=db.query(models.Author).all()
+        logger.info(f"{len(authors)} no of author details are returned")
+        return authors
+    except Exception as e:
+        logger.error(f"Error occured while fetching Author records :{e}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Error "
+        )
 
-def get_author(db:Session,author_id:int):
-    db.query(models.Author).filter(models.Author.id==author_id).first()
+def get_author_by_id(db:Session,author_id:int):
+    author=db.query(models.Author).filter(models.Author.id==author_id).first()
+    if not author:
+        logger.warning(f"this {author_id} does not exist in Authors table")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{author_id} does not exist"
+        )
+    try:
+        logger.info(f"author:{author_id} found")
+        return {"id":author.id,"name":author.name}
+    except Exception as e:
+        logger.error(f"Error while fetching:{e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error"
+        )
+    
     
 def delete_author(db:Session,author_id:int):
-    author=get_author(db,author_id)
-    db.delete(author)
-    db.commit()
+    author=get_author_by_id(db,author_id)
+    if not author:
+        logger.warning(f"this {author_id} does not exist in Authors table")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{author_id} does not exist"
+        )
+    try:
+        db.delete(author)
+        db.commit()
+        logger.info(f"author:{author_id} deleted")
+    except Exception as e:
+        logger.error(f"Error while fetching:{e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error"
+        )
+    
 
-def delete_all_author(db:Session):
-    db.delete(models.Author).all()
+# def update_author(db:Session,author_id:int):
+#     author=db.query(models.Author).filter(models.Author.id==id).first()
+#     if not author:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             details=f'{author_id} does not exist'
+#         )
+#     try:
+        
     
